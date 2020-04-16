017c838be9f7048c38a01ccf340e4989402e422c
Alexandre Cruz <blackbass83@gmail.com>
2020-04-15 21:40:51 -0300

    Add roles validation middleware

---
+++ src/roles.js
@@ -0,0 +1,5 @@
+module.exports = {
+    administradores: 1,
+    estabelecimentos: 2,
+    clientes: 3
+}
--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = {
-    async validate(req, res, next) {
-        const authHeader = req.headers.authorization
+    async authorize(req, res, next) {
+        const { authorization } = req.headers
 
-        if (authHeader) {
-            const token = authHeader.split(' ')[1]
+        if (authorization && authorization.split(' ')[0] === 'Bearer') {
+            const token = authorization.split(' ')[1]
 
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
                 if (err) return res.sendStatus(403)
@@ -14,6 +14,13 @@ module.exports = {
             })
         } else {
             res.sendStatus(401)
+        }
+    },
+
+    hasRole(role) {
+        return (req, res, next) => {
+            if (!req.user.roles.includes(role)) res.sendStatus(403)
+            else next()
         }
     }
 }
