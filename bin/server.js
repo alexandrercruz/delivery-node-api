ce1e68b5712fe46add4c97189f90ac2e0ca43d90
Alexandre Cruz <blackbass83@gmail.com>
2020-04-15 21:41:19 -0300

    Add server.js

---
+++ bin/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const app = require('../src/app');
+
+const port = normalizaPort(process.env.PORT || '3000');
+
+function normalizaPort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+app.use((err, req, res, next) => {
+    console.log(err)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
-require('dotenv').config()
-
 const express = require('express')
+
 const routes = require('./routes')
 
-const port = process.env.PORT
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+module.exports = app
